1ffe592c25930990515e316734810d6dfc09bfaf
tetianaIgorivna <tetiana.ioltukhivska@netcracker.com>
2023-06-11 12:31:40 +0300

    commit 1

---
+++ Src/Homework11.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Homework11 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] organizerNumbers;
+        organizerNumbers = new int[7];
+        for (int i = 0; i < organizerNumbers.length; i++) {
+            organizerNumbers[i] = random.nextInt(9 - 0) + 0;
+        }
+        Arrays.sort(organizerNumbers);
+        System.out.println("Числа фірми-організатора лотереї впорядковані за зростанням: " + Arrays.toString(organizerNumbers));
+        int[] playerNumbers;
+        playerNumbers = new int[7];
+        for (int j = 0; j < playerNumbers.length; j++) {
+            playerNumbers[j] = random.nextInt(9 - 0) + 0;
+        }
+        Arrays.sort(playerNumbers);
+        System.out.println("Числа гравця впорядковані за зростанням:                     " + Arrays.toString(playerNumbers));
+
+        int coincidence = 0;
+        for (int t = 0; t < organizerNumbers.length; t++) {
+            if (organizerNumbers[t] == playerNumbers[t]) {
+                coincidence = coincidence + 1;
+            }
+        }
+        System.out.println("Кількість збігів: " + coincidence);
+    }
+}
+
